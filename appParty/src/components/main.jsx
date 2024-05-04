9eab217fa189c7c51e88c81e036f00b683f53952
serhikalamar <sergimartinez16@gmail.com>
2024-05-04 18:05:08 +0200

    commit 0.03

--- appParty/src/components/main.jsx
+++ appParty/src/components/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Text, Vierw, View } from "react-native";
+import Constants from 'expo-constants'
 
 const Main = () => {
     return(
-        <Vierw>
+        <Vierw style={{
+            marginTop: Constants.statusBarHeight,
+            flexGrow: 1
+        }}>
             <Text>Rate Repository Application</Text>
         </Vierw>
     )
