5d503f8a2fa8ccf53e3c94cc1e6eef78b924e473
serhikalamar <sergimartinez16@gmail.com>
2024-05-04 16:44:17 +0200

    commit 0.01

---
+++ appParty/src/components/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, Vierw, View } from "react-native";
+
+const Main = () => {
+    return(
+        <Vierw>
+            <Text>Rate Repository Application</Text>
+        </Vierw>
+    )
+}
+export default Main
